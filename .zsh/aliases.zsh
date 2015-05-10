615cd4be883e84fea18be43b42af38f0494c7936
zenbro <capybarov@gmail.com>
2015-05-10 19:10:05 +0300

    git aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,6 +10,9 @@ alias ra='ranger'
 alias ya='xmodmap -e "keycode 135 = z Z Cyrillic_ya Cyrillic_YA"'
 alias update_node_bin='n=$(which node);n=${n%/bin/node}; chmod -R 755 $n/bin/*; sudo cp -r $n/{bin,lib,share} /usr/local'
 alias github="git config --local user.name 'zenbro'; git config --local user.email 'capybarov@gmail.com'"
+alias gitc='git add . && git commit'
+alias gita='git add . && git commit --amend'
+alias gitd='git diff HEAD~'
 
 alias lg='nocorrect lg'
 alias vlog='vim <(journalctl --merge -b)'
