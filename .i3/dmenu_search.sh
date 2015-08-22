f9b2821d8818f5c36d42f1e50eede68aad7813b2
zenbro <capybarov@gmail.com>
2015-08-22 10:55:49 +0300

    Fix dmenu web search

--- .i3/dmenu_search.sh
+++ .i3/dmenu_search.sh
@@ -4,5 +4,5 @@ URL='https://duckduckgo.com/?q='
 QUERY=$(echo '' | dmenu -p "Search:" -fn "-xos4-terminus-medium-r-*-*-14-*" -b)
 if [ -n "$QUERY" ]; then
   xdg-open "${URL}${QUERY}" 2> /dev/null
-  exec i3-msg [class="^Firefox$"] focus
+  exec i3-msg '[class="^Google-chrome"] focus'
 fi
